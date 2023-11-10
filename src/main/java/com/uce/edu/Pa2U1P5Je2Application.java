2d0fb23c7aa33c9b9e638af46d569575228e68ed
Jeriko316 <jkenriquez@hotmail.com>
2023-11-10 18:55:56 -0500

    Primer Cambio

---
+++ src/main/java/com/uce/edu/Pa2U1P5Je2Application.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5Je2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5Je2Application.class, args);
+	}
+
+}
