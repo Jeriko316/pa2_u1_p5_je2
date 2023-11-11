7fbdc7ff8fb473ef9ebc513f68199d587a625308
Jeriko316 <jkenriquez@hotmail.com>
2023-11-10 19:13:50 -0500

    taller3

--- src/main/java/com/uce/edu/Pa2U1P5Je2Application.java
+++ src/main/java/com/uce/edu/Pa2U1P5Je2Application.java
@@ -1,13 +1,25 @@
 package com.uce.edu;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Pa2U1P5Je2Application {
+public class Pa2U1P5Je2Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5Je2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+		System.out.println("Soy Jhon Enriquez");
+		System.out.println("cambio en la misma rama");
+		System.out.println("cambio en una nueva rama taller 3");
+		System.out.println("cambio en una nueva rama taller 4");
+
 	}
 
 }
